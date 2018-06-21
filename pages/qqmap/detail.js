e1a43c4546247c8b2a26356883f95e5ec817c0e9
Yunlong.Zhang <Yunlong.Zhang@asaplus.com.cn>
2018-06-21 13:55:04 +0800

    tengxunmap change

---
+++ pages/qqmap/detail.js
@@ -0,0 +1,95 @@
+// pages/qqmap/detail.js
+
+let plugin = requirePlugin("mapRoute");
+
+let routeInfo = {
+  startLat: 0,    //起点经度 选填
+  startLng: 0,    //起点纬度 选填
+  startName: "我的位置",   // 起点名称 选填
+  endLat: 0,    // 终点经度必传
+  endLng: 0,  //终点纬度 必传
+  endName: "",  //终点名称 必传
+  mode: 'car'  //算路方式 选填
+}
+
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    routeInfo: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log("获取的参数:");
+    console.log(options);
+
+    var that = this;
+
+    that.setData({
+      routeInfo: {
+        startLat: parseFloat(options.startLat), //起点经度 选填
+        startLng: parseFloat(options.startLng), //起点纬度 选填
+        startName: "我的位置", // 起点名称 选填
+        endLat: parseFloat(options.endLat), // 终点经度必传
+        endLng: parseFloat(options.endLng), //终点纬度 必传
+        endName: options.endName, //终点名称 必传
+        mode: "car" //算路方式 选填 car，bus，walk
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/qqmap/index.js
+++ pages/qqmap/index.js
@@ -1,78 +1,113 @@
-// pages/qqmap/index.js
+//index.js
+//获取应用实例
+const app = getApp()
+
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    longitude: 0,
+    latitude: 0,
+    endLat:0,
+    endLng:0,
+    endName:''   
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that=this;
+  //页面的初始化  
+  onLoad:function(){
     
-    that.setData({
-      "locInfo": {
-        startLat: 39.90469, //起点经度 选填
-        startLng: 116.40717, //起点纬度 选填
-        startName: "我的位置", // 起点名称 选填
-        endLat: 39.94055, // 终点经度必传
-        endLng: 116.43207, //终点纬度 必传
-        endName: "来福士购物中心", //终点名称 必传
-        mode: "car" //算路方式 选填 car，bus，walk
+    var that=this;
+
+    //定位得到经纬度    
+    wx.getLocation({
+      type: 'gcj02',//wgs84
+      success: function (res) {
+        console.log("得到经纬度坐标:");
+        console.log(res);
+        that.setData({
+          longitude: res.longitude,
+          latitude: res.latitude
+        })
       }
     })
   },
+  play:function(e) {
+    var that=this;
+    console.log("得到经纬度的值:");
+    console.log(e);
+    console.log("位置x:"+e.detail.x+",位置y:"+e.detail.y);
+    var xval = e.detail.x, yval = e.detail.y;
+   
+    
+    if (xval >= 200 && xval <= 280 && yval >= 50 && yval <= 120) {     
+      that.setData({
+        endLat: 31.3835400000,
+        endLng: 121.5274800000,
+        endName: '滨江森林公园' 
+      })     
+      //31.3835400000, 121.5274800000
+    }else if (xval >= 50 && xval<=100&&yval>=35&&yval<=125){ 
+      that.setData({
+        endLat: 31.3417200000,
+        endLng: 121.3767100000,
+        endName: '顾村公园'
+      }) 
+      //31.3417200000,121.3767100000
+    } else if (xval >= 210 && xval <= 265 && yval >= 150 && yval <= 205){  
+      that.setData({
+        endLat: 31.2837100000,
+        endLng: 121.5009500000,
+        endName: '同济大学'
+      }) 
+      //31.2837100000,121.5009500000
+    } else if (xval >= 140&& xval <= 210 && yval >= 170 && yval <= 240) {
+      that.setData({
+        endLat: 31.2717400000,
+        endLng: 121.4832300000,
+        endName: '鲁迅公园'
+      }) 
+      //31.2717400000,121.4832300000
+    } else if (xval >= 115 && xval <=170 && yval >= 260 && yval <= 325) {
+      that.setData({
+        endLat: 31.2339100000,
+        endLng: 121.4625000000,
+        endName: '静安雕塑公园'
+      }) 
+      //31.2339100000,121.4625000000
+    } else if (xval >= 35 && xval <= 115 && yval >= 280 && yval <= 345) {
+      that.setData({
+        endLat: 31.2209500000,
+        endLng: 121.4203200000,
+        endName: '中山公园'
+      })
+      //31.2209500000,121.4203200000
+    } else if (xval >= 220 && xval <= 280 && yval >= 270 && yval <= 335) {
+      that.setData({
+        endLat: 31.2154000000,
+        endLng: 121.5520700000,
+        endName: '世纪公园'
+      })
+      //31.2154000000,121.5520700000
+    } else if (xval >= 140 && xval <= 220 && yval >= 340 && yval <= 410) {
+      that.setData({
+        endLat: 31.2248100000,
+        endLng: 121.4667800000,
+        endName: '延安中路大型公共绿地'
+      })
+      //31.2248100000,121.4667800000
+    } else if (xval >= 100 && xval <= 160 && yval >= 440 && yval <= 520) {
+      that.setData({
+        endLat: 31.1461400000,
+        endLng: 121.4437900000,
+        endName: '上海植物园'
+      })    
+      //31.1461400000,121.4437900000
+    }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    setTimeout(function(){
+      //跳转到导航页面
+      wx.navigateTo({
+        url: '../qqmap/detail?startLat=' + that.data.latitude + '&startLng=' + that.data.longitude + '&endLat=' + that.data.endLat + '&endLng=' + that.data.endLng + '&endName=' + that.data.endName,
+      })
+    },2000)
+   
   }
 })
